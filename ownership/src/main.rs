331ce0bc1b5c04d6fc0aa99a1180c99dfc531d7a
Yogendra Rana <yogendrarana4321@gmail.com>
2023-09-18 11:59:49 +0545

    Ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,23 @@
+struct Book {
+    name: String,
+    price: i32,
+}
+
+fn display_name(book: &Book) {
+    println!("Name: {}", book.name);
+}
+
+
+fn display_price(book: &Book) {
+    println!("Price: {}", book.price);
+}
+
+fn main() {
+    let book1 = Book {
+        name: String::from("Romeo and Juliet"),
+        price: 1200,
+    };
+
+    display_name(&book1);
+    display_price(&book1);
+}
